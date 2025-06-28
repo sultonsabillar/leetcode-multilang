a66f0cc07c595fc2b7e11064ebf46c1fdc09e00f
sultonsabilar <sultonsabillar@gmail.com>
2025-06-28 22:43:05 +0700

    feat(javascript): add two sum solution with usage example

---
+++ javascript/two-sum.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+function twoSum(nums, target) {
+    const map = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+        if (map.has(complement)) {
+            return [map.get(complement), i];
+        }
+        map.set(nums[i], i);
+    }
+    return [];
+}
+
+const result = twoSum([2, 7, 11, 15], 9);
+console.log(result); // Output: [0, 1]
